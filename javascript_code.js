e993057d0280d546578e48176a55e51a8357d9e3
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-09-30 22:06:44 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+  <html>
+  <head>
+  <script>
+      document.addEventListener('DOMContentLoaded', function() {
+        
+      });
+    </script>
+  </head>
+<body>
+  </body>
+</html>
