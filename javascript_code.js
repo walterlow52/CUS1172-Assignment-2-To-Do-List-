b36c3077776c055979ad3f83b13fbd1e4f2503df
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-04 19:10:32 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -8,6 +8,9 @@
       document.addEventListener('DOMContentLoaded', function() {
         document.querySelector("new").onsubmit = function() {
           const li = document.createElement('li');
+          li.innerHTML = document.querySelector('#task').value;
+          document.querySelector("#tasks").append(li);
+          document.querySelector("#task").value = '';
         }
       });
     </script>
