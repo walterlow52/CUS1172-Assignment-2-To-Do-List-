b81b9c74a09429b00d8c369eec6ec9072dfac6ba
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-05 09:39:42 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -3,7 +3,7 @@ alert('Hello toad!');
       document.addEventListener('DOMContentLoaded', function() {
         document.querySelector("new").onsubmit = function() {
           const li = document.createElement('li');
-          //li.innerHTML = document.querySelector('#task').value;
+         li.innerHTML = document.querySelector('#task').value;
          let task_text = document.querySelector('#task').value;
          let new_task_html = `
          <span> ${task_text} </span>
