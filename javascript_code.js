b01a18ea4a62463832266227055bbce800890cf5
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-04 20:07:27 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -8,10 +8,18 @@
       document.addEventListener('DOMContentLoaded', function() {
         document.querySelector("new").onsubmit = function() {
           const li = document.createElement('li');
-          li.innerHTML = document.querySelector('#task').value;
+          //li.innerHTML = document.querySelector('#task').value;
+         let task_text = document.querySelector('#task').value;
+         let new_task_html = `
+         <span> ${task_text} </span>
+         <button class = "remove"> Mark as complete </button>
+         `;
+         li.innerHTML = new_task_html
+         
           document.querySelector("#tasks").append(li);
           document.querySelector("#task").value = '';
           return false;
+         
         }
       });
     </script>
