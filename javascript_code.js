d997383746baf6830f44e8460af51456e508f436
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-05 22:37:38 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -1,3 +1,14 @@
 alert('Welcome to the JavaScript Task List!');
 
+document.addEventListener('DOMContentLoaded', function() {
+  
+document.querySelector("#new").onsubmit = function() {
+  const li = document.createElement('li');
+  li.innerHTML = document.querySelector('#task').value;
+  document.querySelector("#tasks").append(li);
+  document.querySelector("#task").value = '';
+  
+  
+}
+});
 
