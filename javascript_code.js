86c8d0d7ad90a095e54fb35b3d131a5671801192
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-05 22:17:45 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -1,25 +1,5 @@
 alert('Welcome to the JavaScript Task List!');
       
-      document.addEventListener('DOMContentLoaded', function() {
-        document.querySelector("new").onsubmit = function() {
-          const li = document.createElement('li');
-         li.innerHTML = document.querySelector('#task').value;
-         let task_text = document.querySelector('#task').value;
-         let new_task_html = `
-         <span> ${task_text} </span>
-         <button class = "remove"> Mark as complete </button>
-         `;
-         li.innerHTML = new_task_html
-         
-          document.querySelector("#tasks").append(li);
-          document.querySelector("#task").value = '';
-          return false;
-        }
-document.addEventListener('click', function(event) {
- element = event.target;
- 
- if (element.className === 'remove') {
-  element.parentElement.remove();
  }
 });
       });
