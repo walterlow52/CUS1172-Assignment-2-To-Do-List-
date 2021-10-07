08b4b0ca29dea5e8dd2b62f47ed003baf1a60444
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-07 09:37:08 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -23,7 +23,7 @@ document.addEventListener('click', function(event) {
     element.parentElement.remove();
   }
  if (element.className === 'completed') { 
-    document.getElementById("comppleted").innerHTML = "COMPLETED";
+    document.getElementById("completed").innerHTML = "COMPLETED";
   }
   
 })
