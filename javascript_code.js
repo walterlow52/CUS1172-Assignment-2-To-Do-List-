fbf309abc58e5a8f23f11191dc00dbc013067565
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-07 09:26:29 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -8,7 +8,7 @@ document.querySelector("#new").onsubmit = function() {
   let new_task = `
   <span style = "color: white;"> ${task_point} </span> <br>
   <input type = "radio" class = "remove"> <p> Remove from List </p> 
-  <input type = "radio" class = "completed"> <p> Mark as Completed </p>
+  <input type = "radio" class = "completed"> <p style = text-align: right> Mark as Completed </p>
   `;
   li.innerHTML = new_task
   
