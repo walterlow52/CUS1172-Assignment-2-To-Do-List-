f870ef804e1aadccac57689a8cb081c47bf49a62
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-07 10:38:02 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -23,7 +23,7 @@ document.addEventListener('click', function(event) {
     element.parentElement.remove();
   }
  if (element.className === 'completed') { 
-   //element.parentElement.
+   element.parentElement.style.text-decoration = "line-through";
   }
   
 })
