9aff10f418c1e3a631112f50e8e61ca06f037d5b
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-08 17:20:54 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -25,7 +25,7 @@ document.addEventListener('click', function(event) {
   }
  if (element.className === 'completed') { 
    document.getElementById("tasks").style.textDecoration = "line-through";
-   document.querySelector("status").innerHTML = 'COMPLETED';
+   //document.querySelector("status").innerHTML = 'COMPLETED';
   }
   
 })
