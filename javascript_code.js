632eb033cec0859b356be2ffe3153948e91b4bbd
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-08 16:32:30 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -23,7 +23,7 @@ document.addEventListener('click', function(event) {
     element.parentElement.remove();
   }
  if (element.className === 'completed') { 
-   document.getElementById("tasks").style.textDecoration = "line-through";
+   document.getElementById("task").style.textDecoration = "line-through";
   }
   
 })
