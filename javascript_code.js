066e76d08dd87c17b386e7412cfe9f8af738eaa8
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-09 21:02:31 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -5,19 +5,24 @@ document.addEventListener('DOMContentLoaded', function() {
 document.querySelector("#new").onsubmit = function task_list() {
   const li = document.createElement('li');
   let task_point = document.querySelector('#task').value;
+  let task_priority = document.querySelector('#Priority').value;
   let new_task = `
   <h3 id = "status"> PENDING </h3> <br>
-  <span style = "color: white;"> ${task_point} </span>
-  <label for = "priority"> <p> Enter the priority of the task: </p> </label>
-  <select name = "Task Priority" id = "#priority">
+  <span style = "color: white;"> ${task_point} </span> <br>
+  <span style = "text-align: center;> ${task_priority} </span>
+  <input type = "radio" class = "remove"> <p> Remove from List </p>
+  <input type = "radio" class = "completed"> <p> Mark as Completed </p>
+  `;
+  let priority_check = `
+  <label for = "priority" class = "Priority"> <p> Enter the priority of the task: </p> </label> <br>
+  <select name = "Task Priority">
   <option value = "Low"> Low </option>
   <option value = "Medium"> Medium </option>
   <option value = "High"> High </option>
   </select> <br>
-  <input type = "radio" class = "remove"> <p> Remove from List </p>
-  <input type = "radio" class = "completed"> <p> Mark as Completed </p>
   `;
   li.innerHTML = new_task
+  priority.innerHTML = priority_check;
   
   document.querySelector("#tasks").append(li);
   document.querySelector("#task").value = '';
@@ -33,6 +38,7 @@ document.querySelector("#new").onsubmit = function task_list() {
    document.getElementById("tasks").style.textDecoration = "line-through";
    document.querySelector("#status").innerHTML = "COMPLETED";
   }
-  
+ if (element.className === 'priority') 
+    
 })
 });
