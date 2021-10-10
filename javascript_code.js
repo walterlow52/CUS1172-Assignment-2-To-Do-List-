026f286e0e09f8404a5f137f99851b7fd6589e16
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2021-10-09 21:19:25 -0400

    Update javascript_code.js

--- javascript_code.js
+++ javascript_code.js
@@ -38,7 +38,7 @@ document.querySelector("#new").onsubmit = function task_list() {
    document.querySelector("#status").innerHTML = "COMPLETED";
   }
  if (element.className === 'priority') {
-   document.querySelector("#Priority").value = task_priority;
+   document.querySelector("#Priority").value = 'task_priority';
  }
     
 })
